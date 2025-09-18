a2fd1232cad96e527598418460d2d32de2dfbb05
Caio Ribeiro <69832122+CaioRibeiroDev@users.noreply.github.com>
2025-09-18 10:58:41 +0000

    finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,24 @@
 import database from "infra/database.js"
 
 export default async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;')
-  console.log(result.rows)
-  response.status(200).json({chave: "valor"})
+  const updatedAt = new Date().toISOString();
+
+  const queryResultPostgresVersion = await database.query('SELECT version();');
+  const postgresVersion = await queryResultPostgresVersion.rows[0].version;
+
+  const queryResultMaxConnections = await database.query('SHOW max_connections;');
+  const maxConnections = Number(queryResultMaxConnections.rows[0].max_connections);
+
+  const usedConnections = await database.getUsedConnections()
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        postgres_version: postgresVersion,
+        max_connections: maxConnections,
+        used_connections: usedConnections
+      }
+    },
+  })
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status")
-  expect(response.status).toBe(200)
-})
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString()
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt)
+
+  
+  expect(responseBody.dependencies.database.postgres_version).toBeDefined()
+  expect(typeof responseBody.dependencies.database.max_connections).toBe('number')
+  expect(typeof responseBody.dependencies.database.used_connections).toBe('number')
+  console.log(responseBody)
+  })
