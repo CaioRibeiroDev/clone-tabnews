3d633e8b29323e2d83d4b21284a06b8735506115
Caioribeiro.dev <caioribeirodev@gmail.com>
2025-11-19 16:31:25 -0300

    fix `prettier` style `linting`

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,18 @@
-import { Client } from 'pg'
-import dns from 'dns'
+import { Client } from "pg";
+import dns from "dns";
 
-dns.setDefaultResultOrder('ipv4first');
+dns.setDefaultResultOrder("ipv4first");
 
 async function query(queryObject) {
   let client;
-  
+
   try {
     client = await getNewClient();
     const result = await client.query(queryObject);
-   
+
     return result;
   } catch (error) {
-    console.error(error)
+    console.error(error);
     throw error;
   } finally {
     await client.end();
@@ -20,38 +20,38 @@ async function query(queryObject) {
 }
 
 async function getNewClient() {
-    const client = new Client({
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      ssl: getSSLValues(),
-    });
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    ssl: getSSLValues(),
+  });
 
-    await client.connect();
-    return client;
-}  
+  await client.connect();
+  return client;
+}
 
 function getSSLValues() {
   if (process.env.POSTGRES_CA) {
     return {
       ca: process.env.POSTGRES_CA,
-    }
+    };
   }
 
-  return process.env.NODE_ENV === 'production' ?  true : false
+  return process.env.NODE_ENV === "production" ? true : false;
 }
 
 export default {
   query,
   getNewClient,
   getUsedConnections: async () => {
-    const databaseName = 'local_db'
+    const databaseName = "local_db";
     const result = await query({
       text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-      values: [databaseName]
+      values: [databaseName],
     });
-    return result.rows[0].count
-  }
-}
+    return result.rows[0].count;
+  },
+};
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,20 +1,23 @@
-import orchestrator from 'tests/orchestrator.js'
+import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-})
+});
 
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString()
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt)
 
-  
-  expect(responseBody.dependencies.database.postgres_version).toBeDefined()
-  expect(typeof responseBody.dependencies.database.max_connections).toBe('number')
-  expect(typeof responseBody.dependencies.database.used_connections).toBe('number')
-  })
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.postgres_version).toBeDefined();
+  expect(typeof responseBody.dependencies.database.max_connections).toBe(
+    "number",
+  );
+  expect(typeof responseBody.dependencies.database.used_connections).toBe(
+    "number",
+  );
+});
